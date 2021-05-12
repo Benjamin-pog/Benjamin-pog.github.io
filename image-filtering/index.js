ecafad9cc63329dd6b29f3b8e0607dd0737cf65d
Benjamin-pog <benjamin.d.abernathy@gmail.com>
2021-05-12 18:49:54 -0500

    almost

--- image-filtering/index.js
+++ image-filtering/index.js
@@ -19,6 +19,8 @@ $(document).ready(function(){
 /////////////////////////////////////////////////////////
 
 // TODO 1 & 3: Create the applyFilter function here
+
+/*
 function applyFilter(filterFunction) {
     for (var o = 0; o < image.length; o++) {
         for (var p = 0;  p < image[o].length; p++) {
@@ -30,17 +32,18 @@ function applyFilter(filterFunction) {
         }
     }
 }
+*/
 
 // TODO 5: Create the applyFilterNoBackground function
 
-function applyFilterNoBackground(filterfunction) {
+function applyFilterNoBackground(filterFunction) {
     for (var j = 0; j < image.length; j++) {
         for (var k = 0; k < image[j].length; k++) {
             if (image[RED] === 150 && image[BLUE] === 150 && image[GREEN] === 150) {
                 image[RED] = 150;
                 image[BLUE] = 150;
                 image[GREEN] = 150;
-            } else {    
+            } if (image[RED, BLUE, GREEN] !== 150) {    
                 rgbString = image[j][k];
                 var rgbNumbers = rgbStringToArray(rgbString);
                 filterFunction(rgbNumbers);
